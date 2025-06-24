70dd0a00aeae07a645d177f1e6c85b4eca2cfe65
Amam Raushan <amanraushan0@gmail.com>
2025-06-24 23:21:33 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
                 <li><a href="#">Menu</a></li>
                 <li><a href="#">Location</a></li>
                 <li><a href="#">About</a></li>
-                <li><a href="#">Contact</a></li>
+                <li><a href="Contact.jsx">Contact</a></li>
             </ul>
             <button className="btn">Login</button>
         </nav>
